d0089ebeb5317a04f6ab6518a4526e807d50af14
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-05 19:00:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagram)
+    anagram.select {|i| i.chars.sort == word.chars.sort}
+  end
+
+end
